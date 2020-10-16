29542f20683daf74b42a12f90b4aadaf8cfc8035
reidy <reidy@alodokter.com>
2020-10-16 19:32:02 +0700

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,34 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_reader :guesses, :wrong_guesses, :word
+
+  NUM_OF_GUESS = 7
+
   def initialize(word)
+    raise ArgumentError.new('invalid word') unless word.is_a?(String)
     @word = word
+    @wrong_guesses = ''
+    @guesses = ''
   end
+
+  def guess(letter)
+    raise ArgumentError.new('invalid letter') unless (letter.is_a?(String) and letter.match?(/^[^\W\d]+/))
+    val = letter.downcase
+    return false if losed?
+    if (@guesses+@wrong_guesses).downcase.include?(val)
+      false
+    elsif @word.include?(val)
+      @guesses += val
+      true
+    else
+      @wrong_guesses += val
+      true
+    end
+  end
+
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -22,6 +46,26 @@ class HangpersonGame
     Net::HTTP.new('watchout4snakes.com').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+  def word_with_guesses
+    @word.gsub(/[^#{@guesses}\s]/i,'-')
+  end
+
+  def check_win_or_lose
+    if @word.gsub(/[#{@guesses}\s]/i,'').length == 0
+      :win
+    elsif losed?
+      :lose
+    else
+      :play
+    end
+  end
+
+  private
+
+  def losed?
+    @wrong_guesses.length >= NUM_OF_GUESS
   end
 
 end
